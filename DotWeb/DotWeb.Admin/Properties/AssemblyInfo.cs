e43656b2f0fae3859fb0d915f14315b6bc5f0f8d
Agus Suhanto <agus.suhanto@gmail.com>
2016-10-11 10:47:24 +0700

    Ready as as version 0.1.0
    Please merge to master

--- DotWeb/DotWeb.Admin/Properties/AssemblyInfo.cs
+++ DotWeb/DotWeb.Admin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotWeb_Admin")]
+[assembly: AssemblyTitle("DotWeb Admin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotWeb_Admin")]
-[assembly: AssemblyCopyright("Copyright ? 2016")]
+[assembly: AssemblyCompany("DotSystem Indonesia")]
+[assembly: AssemblyProduct("DotWeb Admin")]
+[assembly: AssemblyCopyright("Copyright © DotSystem 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

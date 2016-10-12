d00938ebb597aeb6e07abb9633e73c7f242c9644
Agus Suhanto <agus.suhanto@gmail.com>
2016-10-12 12:25:18 +0700

    Revert "Fix web.admin different connection string names"
    This reverts commit 021b2916e6947ac359b2206975fa1d654acb4798.

--- DotWeb/DotWeb.Admin/Root.master.designer.cs
+++ DotWeb/DotWeb.Admin/Root.master.designer.cs
@@ -13,15 +13,6 @@ namespace DotWeb_Admin {
     public partial class RootMaster {
         
         /// <summary>
-        /// PageTitle control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder PageTitle;
-        
-        /// <summary>
         /// Body control.
         /// </summary>
         /// <remarks>
